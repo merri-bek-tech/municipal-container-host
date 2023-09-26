253408d9db5faec2b7cdeeeb6169f26899fd867e
Craig Ambrose <craig@craigambrose.com>
2023-09-26 21:23:59 +1000

    enables docuwiki user management

--- roles/apps/dokuwiki/templates/local.php
+++ roles/apps/dokuwiki/templates/local.php
@@ -5,3 +5,4 @@ $conf['userewrite']  = 1;                //this makes nice URLs: 0: off 1: .htac
 $conf['useslash']    = 1;                //use slash instead of colon? only when rewrite is on
 $conf['sepchar']     = '_';              //word separator character in page names; may be a
                                          //  letter, a digit, '_', '-', or '.'.
+$conf['useacl']      = 1;
---
+++ roles/apps/dokuwiki/templates/users.auth.php
@@ -0,0 +1,11 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+admin:$2y$10$P5YH8uIM2uAE9snRq32yAuHMb4/XAzksFd5Cakqqtsw9BWeSsyLZq:admin:admin@admin.com:admin,user
